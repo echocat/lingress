05136ac817cd8930b91e1ac85dcca085a3e19b2d
Gregor Noczinski <gregor@noczinski.eu>
2024-08-20 11:06:37 +0200

    Update dependencies

--- support/slf4g_native/hook.go
+++ support/slf4g_native/hook.go
@@ -1,6 +1,8 @@
 package slf4g_native
 
 import (
+	"context"
+	"fmt"
 	"github.com/echocat/slf4g"
 	"github.com/echocat/slf4g-klog/bridge"
 	_ "github.com/echocat/slf4g-klog/bridge/hook"
@@ -10,7 +12,13 @@ import (
 func init() {
 	kl := log.GetLogger("kubernetes")
 	bridge.ConfigureWith(kl)
-	runtime.ErrorHandlers = []func(err error){func(err error) {
-		kl.WithError(err).Error("Unhandled kubernetes internal error.")
+	runtime.ErrorHandlers = []runtime.ErrorHandler{func(_ context.Context, err error, msg string, keysAndValues ...interface{}) {
+		out := "Unhandled kubernetes internal error"
+		if msg != "" {
+			out += ": " + fmt.Sprintf(msg, keysAndValues...)
+		} else {
+			out += "."
+		}
+		kl.WithError(err).Error(out)
 	}}
 }
